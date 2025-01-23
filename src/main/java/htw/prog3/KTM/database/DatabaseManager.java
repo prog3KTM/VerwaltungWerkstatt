f8b0714eb6fa765a95eff35e72b2ecf0ea25ae01
Nico Kopp <koppy.nico@gmail.com>
2025-01-23 10:49:54 +0100

    DatabaseManager no longer creates connection for each Task

--- src/main/java/htw/prog3/KTM/database/DatabaseManager.java
+++ src/main/java/htw/prog3/KTM/database/DatabaseManager.java
@@ -12,15 +12,23 @@ public class DatabaseManager {
     private static final String URL = "jdbc:sqlite:werkstatt.db"; // SQLite-Datenbank-URL
     private static final String USERNAME = ""; // Falls notwendig, Benutzername
     private static final String PASSWORD = ""; // Falls notwendig, Passwort
+    private static Connection CONNECTION = null;
+
+    public DatabaseManager() {
+        try {
+            CONNECTION = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not connect to database", e);
+        }
+    }
 
     // Stellt eine Verbindung zur SQLite-Datenbank her
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public Connection getConnection() {
+        return CONNECTION;
     }
 
     // Stellt einen DSLContext bereit, der für die Interaktion mit der Datenbank verwendet wird
     public DSLContext getDSLContext() throws SQLException {
-        Connection connection = getConnection();
-        return DSL.using(connection);
+        return DSL.using(CONNECTION);
     }
 }
--- src/main/java/htw/prog3/KTM/model/WerkstattInformation.java
+++ src/main/java/htw/prog3/KTM/model/WerkstattInformation.java
@@ -1,4 +1,7 @@
 package htw.prog3.KTM.model;
 
-public class WerkstattInformation {
-}
+import java.util.List;
+
+public record WerkstattInformation(String name, String location, int phone, String email, String website,
+                                   List<String> offeredServices, String vat, String businessRegNumber,
+                                   String iban) { }
